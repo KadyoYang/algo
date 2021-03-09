fda41465c8b4ce03d9269b847c4f1711b0831c5b
JinDogNOOB <mrriey00@naver.com>
2021-03-09 19:59:01 +0900

    add dp01 설계
    인터넷 잘 들어오는 집에가서 풀기

---
+++ ref/programmers_basic/07_dp/01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/*
+n으로표현
+n 1~9 숫자와 사칙연산만으로 특정수 number를 만드는
+n의 최소사용횟수 리턴
+
++ - * /
+최솟값 8보다 크면 -1리턴
+
+
+
+1: 5 
+2: 55 1번째것들 사칙연산...
+3: 555 2번째것들 사칙연산...
+4: 5555 3번째것들 사칙연산...
+
+각 번호의 0번째는 제외
+이미 계산한 값이 또 나오면 해당 값 제외
+거의 넓이탐색 + dfs 
+해봐라
+
+*/
+
+
+using namespace std;
+
+
+
+int solution(int N, int number){
+    int answer = 0;
+    return answer;
+}
+
