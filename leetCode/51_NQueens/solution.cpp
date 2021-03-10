497e76bf8e625ee09cb531da1f1398bb18fd9649
JinDogNOOB <mrriey00@naver.com>
2021-03-10 21:35:44 +0900

    nothing

---
+++ leetCode/51_NQueens/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+// the n-queens puzzle is the problem of placing n queens on a nXn chessboard such that no two queens attack each other
+// given an integer n, return all distinct solutions to the n-queens puzzle
+// each solution contains a distinct board configuration of the n-queens placement where Q & . both indicate a queen and an empty space respectively
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        
+    }
+};
---
+++ ref/debuggingTest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+
+
+using namespace std;
+
+void printsomething(){
+    cout << "adf" << endl;
+    cout << "1111" << endl;
+}
+
+int main(){
+    printsomething();
+    return 0;
+}
