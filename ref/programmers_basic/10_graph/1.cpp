d30fe36865d6b953c89d54e92ad6e6f942e95789
devteam1 <mrriey00@naver.com>
2021-11-17 19:09:17 +0900

    쓸모없는 함수 제거

--- ref/programmers_basic/10_graph/1.cpp
+++ ref/programmers_basic/10_graph/1.cpp
@@ -13,55 +13,46 @@ return 가장 먼 노드 갯수
 
 */
 
-unordered_set<int> getLinkedNode(vector<vector<int>>& edge, unordered_set<int>& usedNode, int target){
-    unordered_set<int> result;
-
-    for(vector<int> vertex : edge){
-        if(vertex[0] == target && (usedNode.find(vertex[1]) == usedNode.end())){
-            result.insert(vertex[1]);
-        }else if(vertex[1] == target && (usedNode.find(vertex[0]) == usedNode.end())){
-            result.insert(vertex[0]);
-        }else{
-
-        }
-    }
-
-    return result;
-}
 
 
-int solution(int n, vector<vector<int>> edge) {
+int solution(int n, vector<vector<int>> edge)
+{
     int answer = 0;
-    
+
     vector<unordered_set<int>> nodeLevel;
     unordered_set<int> usedNode;
-    
+
     nodeLevel.push_back({1});
     usedNode.insert(1);
 
-
-    while(!nodeLevel.back().empty()){
+    while (!nodeLevel.back().empty()){
         unordered_set<int> levelToSave;
-        for(int i : nodeLevel.back()){
-            unordered_set<int> temp = getLinkedNode(edge, usedNode, i);
-
-            for(int val : temp){
-                usedNode.insert(val);
-                levelToSave.insert(val);
+        for (int i : nodeLevel.back()){
+            for (vector<int> vertex : edge){
+                if (vertex[0] == i && (usedNode.find(vertex[1]) == usedNode.end())){
+                    usedNode.insert(vertex[1]);
+                    levelToSave.insert(vertex[1]);
+                }else if (vertex[1] == i && (usedNode.find(vertex[0]) == usedNode.end())){
+                    usedNode.insert(vertex[0]);
+                    levelToSave.insert(vertex[0]);
+                }else{}
             }
         }
+
 
         nodeLevel.push_back(levelToSave);
     }
 
     nodeLevel.pop_back();
     answer = nodeLevel.back().size();
+    
+
 
     return answer;
 }
 
-
-int main(){
+int main()
+{
 
     vector<vector<int>> dat{{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
     int a = 6;
