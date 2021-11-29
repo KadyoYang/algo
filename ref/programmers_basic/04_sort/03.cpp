6a06cc1676924f1017daf3606840729f7bfbca01
devteam1 <mrriey00@naver.com>
2021-11-29 12:06:17 +0900

    add better solution

--- ref/programmers_basic/04_sort/03.cpp
+++ ref/programmers_basic/04_sort/03.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 /*
@@ -33,4 +34,29 @@ int solution(vector<int> citations) {
 
     }
     return answer;
+}
+
+
+// better solution 
+
+int solution_1(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+
+    return citations.size();
+}
+
+
+int main(){
+
+    vector<int> a{3, 0, 6, 1, 5};
+    
+    cout << solution_1(a) << endl;
+
+    return 0;
 }
