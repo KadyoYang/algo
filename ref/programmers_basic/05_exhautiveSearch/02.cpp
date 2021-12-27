746dfb49bafda9c6d85ea1814fd751a88396017a
Kadyo Yang <mrriey00@naver.com>
2021-12-27 15:42:35 +0900

    add Joystick class

---
+++ ref/programmers_basic/05_exhautiveSearch/02.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Joystick {
+private:
+    int currentPos = 'A';
+    int start = 'A';
+    int end = 'Z';
+    bool isItOkayToGoForward(char destinationChar){
+        
+    }
+public:
+    int movePositionTo(char destinationChar){
+
+    }
+
+}
+
+
+
+int solution(string name) {
+    int answer = 0;
+    return answer;
+}
+
+
+
